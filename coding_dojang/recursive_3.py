3815948796cdbdf70a7c399c370d593be3c8b330
신 종민 <standbyme227@algorithmlabs.co.kr>
2019-06-28 08:56:18 +0900

    python 공부190628 c1

--- coding_dojang/recursive_3.py
+++ coding_dojang/recursive_3.py
@@ -1,1 +1,17 @@
-def ssss(order):
+def fib(number):
+    count = 1
+    a = 0
+    b = 1
+    c = 0
+    if number <= 1:
+        return number
+    else:
+        while number > count:
+            c = a + b
+            a = b
+            b = c
+            count += 1
+    return c
+
+
+print(fib(10))
